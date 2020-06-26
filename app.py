74472d7435faf62a38f66f95146a05f7c4b78ca4
NaNdalal-dev <56131112+NaNdalal-dev@users.noreply.github.com>
2020-06-27 00:35:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,59 @@
 from flask import *
 import wikipedia as wk
 app=Flask(__name__)
+
 @app.route('/')
 def home():
 	return render_template('index.html',title='PocketPedia')
 
 @app.route('/result',methods=['POST'])
 def res():
-	try:
-		lang=request.form['lang']
-		wk.set_lang(lang)
-		srh=request.form['search']
-		q=wk.page(str(srh))
-		related=q.links[:4]
-		title=q.original_title
-		srh_res=wk.summary(srh)
+		try:
+			global lang,srh,q,fullink,title,srh_res,related_search
+			lang=request.form['lang']
+			wk.set_lang(lang)
+			srh=request.form['search']
+			q=wk.page(str(srh))
+			fullink=q.url
+			title=q.original_title
+			srh_res=wk.summary(srh)
+			related_search=wk.search(title)
+			'''rs=[]
+			title_rs=[]
+			for i in range(len(related_search)):
+				try:
+					rs.append(wk.summary(related_search[i]))
+					title_rs.append(related_search[i])
+				except:
+					pass'''
 
-		return render_template('result.html',title=title,summary=srh_res,related=related)
-	except ConnectionError:
-		return '<center><h3 style="color:red;">Make sure your internet is connected</h3></center>'
-	except:
-			return '''<center><h3 style="color:red;">
 
-			This query doesn\'t exist in our file make sure you type full name <br> or else make sure you won\'t type any special characters,symbols,numerals
+			return render_template('result.html',title=title,summary=srh_res,fullink=fullink)
+		except:
+			return "<h3 style='color:red;'><center>Error! page not found </center></h3>"
 
-			</h3></center>'''
+@app.route('/related_search',methods=['POST'])
+def related_srh():
+			rs=[]
+			title_rs=[]
+			l=0
+			for i in range(5):
+				try:
+					rs.append(wk.summary(related_search[i]))
+					title_rs.append(related_search[i])
+					l+=1
+				except:
+					pass
+			return render_template('related.html',title_rs=title_rs,related_search=rs,l=l)
 
-	#except:
-	#	return '<h3 style="color:red;">Sorry! This query doesn\'t exist in our page</h3>'
+
+@app.route('/result')
+def not_found():
+	return '<h3 style="color:red;"><center>Error 404!page not found <br>  Go to main <a href="/">page</a></center></h3>'
+@app.route('/related_search')
+def not_found2():
+	return '<h3 style="color:red;"><center>Error 404!page not found <br>  Go to main <a href="/">page</a><center></h3>'
+
 
 if(__name__=='__main__'):
 	app.run(debug=True)
